6a2eb6f9e93ce87f26190e3147bebc97ee0d13c2
Ion Berasaluce <ion.berasaluce16@gmail.com>
2020-05-05 22:11:55 +0100

    Remove Actor class

--- ArcadeApp/App/Actor.cpp
+++
@@ -1,8 +0,0 @@
-#include "Actor.h"
-
-
-void Actor::Init(const SpriteSheet& spriteSheet, const std::string& animationsPath, const Vec2D& initialPos, const Colour& spriteColour)
-{
-	m_Sprite.Init(animationsPath, spriteSheet, spriteColour);
-	m_Sprite.SetPosition(initialPos);
-}
